13edea02e5b0d8a9f1fc1b4d613864d5ebb9d363
Reuben Mangubat <reuben@newlogic.com>
2020-10-16 01:02:01 +0800

    Add checking of camera flash availability

--- mlkit/src/main/java/com/newlogic/mlkitlib/newlogic/utils/CameraUtils.kt
+++ mlkit/src/main/java/com/newlogic/mlkitlib/newlogic/utils/CameraUtils.kt
@@ -2,22 +2,22 @@ package com.newlogic.mlkitlib.newlogic.utils
 
 import android.content.Context
 import android.content.pm.PackageManager
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 
 object CameraUtils {
     private val TAG = CameraUtils::class.java.simpleName
 
     fun isLedFlashAvailable(context: Context): Boolean {
-        // method 1
         if (context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
             return true
         }
-        // method 2
-       /* val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+       val cameraManager = context.getSystemService(Context.CAMERA_SERVICE) as CameraManager
         for (id in cameraManager.cameraIdList) {
             if (cameraManager.getCameraCharacteristics(id).get(CameraCharacteristics.FLASH_INFO_AVAILABLE) == true) {
                 return true
             }
-        }*/
+        }
         return false
     }
 }
