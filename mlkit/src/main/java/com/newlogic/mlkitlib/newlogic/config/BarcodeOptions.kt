c43cb15e1fc71ac4915456411854baa310eeb5f6
Reuben Mangubat <reuben@newlogic.com>
2020-10-22 15:18:23 +0800

    Refactor redundant barcode format and arrange them alphabetically

--- mlkit/src/main/java/com/newlogic/mlkitlib/newlogic/config/BarcodeOptions.kt
+++ mlkit/src/main/java/com/newlogic/mlkitlib/newlogic/config/BarcodeOptions.kt
@@ -6,26 +6,25 @@ import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 object BarcodeOptions {
     private val TAG = BarcodeOptions::class.java.simpleName
 
+    val ALL : BarcodeScannerOptions = BarcodeScannerOptions.Builder()
+        .setBarcodeFormats(
+            Barcode.FORMAT_ALL_FORMATS
+        ).build()
+
     val COMMON : BarcodeScannerOptions = BarcodeScannerOptions.Builder()
         .setBarcodeFormats(
-            Barcode.FORMAT_CODE_128,
+            Barcode.FORMAT_AZTEC,
+            Barcode.FORMAT_CODABAR,
             Barcode.FORMAT_CODE_39,
             Barcode.FORMAT_CODE_93,
-            Barcode.FORMAT_CODABAR,
+            Barcode.FORMAT_CODE_128,
             Barcode.FORMAT_DATA_MATRIX,
-            Barcode.FORMAT_EAN_13,
             Barcode.FORMAT_EAN_8,
+            Barcode.FORMAT_EAN_13,
             Barcode.FORMAT_ITF,
             Barcode.FORMAT_QR_CODE,
             Barcode.FORMAT_UPC_A,
             Barcode.FORMAT_UPC_E,
-            Barcode.FORMAT_QR_CODE,
-            Barcode.FORMAT_AZTEC
-        ).build()
-
-    val QR_CODE : BarcodeScannerOptions = BarcodeScannerOptions.Builder()
-        .setBarcodeFormats(
-            Barcode.FORMAT_QR_CODE
         ).build()
 
     val PDF_417 : BarcodeScannerOptions = BarcodeScannerOptions.Builder()
@@ -33,8 +32,8 @@ object BarcodeOptions {
             Barcode.FORMAT_PDF417
         ).build()
 
-    val ALL : BarcodeScannerOptions = BarcodeScannerOptions.Builder()
+    val QR_CODE : BarcodeScannerOptions = BarcodeScannerOptions.Builder()
         .setBarcodeFormats(
-            Barcode.FORMAT_ALL_FORMATS
+            Barcode.FORMAT_QR_CODE
         ).build()
 }
