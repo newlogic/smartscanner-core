5bdd28fe33c3633102423077455fa0cc9b5be179
Reuben Mangubat <reuben@newlogic.com>
2020-11-12 08:44:37 +0800

    Update UI/UX design of demo app

--- core/demo/src/main/java/com/newlogic/mlkit/demo/MainActivity.kt
+++ core/demo/src/main/java/com/newlogic/mlkit/demo/MainActivity.kt
@@ -1,102 +1,85 @@
 package com.newlogic.mlkit.demo
 
 import android.annotation.SuppressLint
+import android.content.ActivityNotFoundException
 import android.content.Intent
-import android.graphics.BitmapFactory
-import android.graphics.Paint
 import android.os.Bundle
 import android.util.Log
-import android.view.View
-import android.view.View.GONE
-import android.view.View.VISIBLE
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomsheet.BottomSheetDialog
-import com.google.gson.JsonParser
 import com.newlogic.mlkit.R
 import com.newlogic.mlkit.databinding.ActivityMainBinding
-import com.newlogic.mlkit.demo.utils.AnimationUtils
+import com.newlogic.mlkit.demo.ResultActivity.Companion.SCAN_RESULT
 import com.newlogic.mlkitlib.newlogic.MLKitActivity
 import com.newlogic.mlkitlib.newlogic.config.Config
 import com.newlogic.mlkitlib.newlogic.config.ImageResultType.PATH
 import com.newlogic.mlkitlib.newlogic.config.Modes
 import com.newlogic.mlkitlib.newlogic.config.MrzFormat
-import com.newlogic.mlkitlib.newlogic.extension.decodeBase64
 import com.newlogic.mlkitlib.newlogic.extension.empty
 import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val TAG = "Newlogic-MainActivity"
+        private const val OP_MLKIT = 1001
+        val imageType = PATH.value
+
+        private fun sampleConfig() = Config(
+                branding = true,
+                background = String.empty(),
+                font = String.empty(),
+                imageResultType = imageType,
+                label = String.empty(),
+                isManualCapture = true
+        )
+    }
+
     private lateinit var binding : ActivityMainBinding
 
-    override fun onCreate (savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
     }
 
-    public override fun onActivityResult (requestCode: Int, resultCode: Int, intent: Intent?) {
+    override fun onStart() {
+        super.onStart()
+        binding.itemMrz.item.setOnClickListener { startIntentCallOut() }
+        binding.itemQr.item.setOnClickListener { startQrScan() }
+        binding.itemBarcode.item.setOnClickListener { startBarcodeScan() }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
         super.onActivityResult(requestCode, resultCode, intent)
         if (requestCode == OP_MLKIT) {
             Log.d(TAG, "Plugin post ML Activity resultCode $resultCode")
             if (resultCode == RESULT_OK) {
-                val returnedResult = intent?.getStringExtra(MLKitActivity.MLKIT_RESULT)
-                binding.textLabelEmpty.visibility = GONE
-                returnedResult?.let { setupResultView(it) }
+                val result = intent?.getStringExtra(MLKitActivity.MLKIT_RESULT)
+                val resultIntent = Intent(this, ResultActivity::class.java)
+                resultIntent.putExtra(SCAN_RESULT, result)
+                startActivity(resultIntent)
             }
         }
     }
 
-    private fun setupResultView (result: String) {
-        val resultObject = JsonParser.parseString(result).asJsonObject
-        val originalHeight = 750 // Approx. 250dp for image and textView
-        // Result Details
-        binding.textResultDetails.visibility = VISIBLE
-        // Image
-        if (resultObject["image"] != null) {
-            val image = resultObject["image"].asString
-            binding.imageView.setImageBitmap(if (imageType == PATH.value) BitmapFactory.decodeFile(image) else image.decodeBase64())
-            binding.imageView.visibility = VISIBLE
-            binding.txtImgAction.visibility = VISIBLE
-            binding.txtImgAction.paintFlags = binding.txtImgAction.paintFlags or Paint.UNDERLINE_TEXT_FLAG
-            binding.txtImgAction.setOnClickListener {
-                AnimationUtils.expandCollapse(binding.imageView, originalHeight)
-                binding.txtImgAction.text = if (binding.imageView.visibility == GONE) getString(R.string.action_show_hide) else getString(R.string.action_show)
-            }
-        } else {
-            binding.imageView.visibility = GONE
-            binding.txtImgAction.visibility = GONE
-        }
-        // Simple Data
-        if (resultObject["givenNames"] != null) {
-            binding.textName.visibility = VISIBLE
-            binding.textName.text = getString(R.string.label_name, resultObject["givenNames"].asString.toLowerCase(Locale.ROOT).capitalize())
-        } else binding.textName.visibility = GONE
-        if (resultObject["surname"] != null) {
-            binding.textSurName.visibility = VISIBLE
-            binding.textSurName.text = getString(R.string.label_surname, resultObject["surname"].asString.toLowerCase(Locale.ROOT).capitalize())
-        } else binding.textSurName.visibility = GONE
-        if (resultObject["dateOfBirth"] != null) {
-            binding.textBirthday.visibility = VISIBLE
-            binding.textBirthday.text = getString(R.string.label_birthday, resultObject["dateOfBirth"].asString)
-        } else binding.textBirthday.visibility = GONE
-        if (resultObject["nationality"] != null) {
-            binding.textNationality.visibility = VISIBLE
-            binding.textNationality.text = getString(R.string.label_nationality, resultObject["nationality"].asString)
-        } else binding.textNationality.visibility = GONE
-        //Raw Data
-        binding.editTextTextMultiLine.setText(result)
-        binding.editTextTextMultiLine.visibility = VISIBLE
-        binding.txtRawDataAction.visibility = VISIBLE
-        binding.txtRawDataAction.paintFlags = binding.txtRawDataAction.paintFlags or Paint.UNDERLINE_TEXT_FLAG
-        binding.txtRawDataAction.setOnClickListener {
-            AnimationUtils.expandCollapse(binding.editTextTextMultiLine, originalHeight)
-            binding.txtRawDataAction.text = if (binding.editTextTextMultiLine.visibility == GONE) getString(R.string.action_show_hide) else getString(R.string.action_show)
+    private fun startIntentCallOut() {
+        try {
+            val intent = Intent("com.newlogic.mlkitlib.SCAN")
+            intent.putExtra(MLKitActivity.MODE, Modes.MRZ.value)
+            intent.putExtra(MLKitActivity.MRZ_FORMAT, MrzFormat.MRTD_TD1.value)
+            intent.putExtra(MLKitActivity.CONFIG, sampleConfig())
+            startActivityForResult(intent, OP_MLKIT)
+        } catch (ex: ActivityNotFoundException) {
+            ex.printStackTrace()
+            Log.e(TAG, "smart scanner is not installed!")
         }
     }
 
-    fun startScanningActivity (view: View) {
+    private fun startMrzScan() {
         val intent = Intent(this, MLKitActivity::class.java)
         intent.putExtra(MLKitActivity.MODE, Modes.MRZ.value)
         intent.putExtra(MLKitActivity.MRZ_FORMAT, MrzFormat.MRTD_TD1.value)
@@ -104,13 +87,13 @@ class MainActivity : AppCompatActivity() {
         startActivityForResult(intent, OP_MLKIT)
     }
 
-    fun startQRCodeScanningActivity (view: View) {
+    private fun startQrScan() {
         val qrFormatOnly = arrayListOf("QR_CODE")
         startBarcode(qrFormatOnly)
     }
 
     @SuppressLint("InflateParams")
-    fun startBarcodeScanningActivity (view: View)  {
+    private fun startBarcodeScan()  {
         val bottomSheetDialog = BottomSheetDialog(this)
         val sheetViewBarcode = layoutInflater.inflate(R.layout.bottom_sheet_barcode, null)
         bottomSheetDialog.setContentView(sheetViewBarcode)
@@ -132,26 +115,11 @@ class MainActivity : AppCompatActivity() {
         bottomSheetDialog.show()
     }
 
-    private fun startBarcode (barcodeOptions : ArrayList<String>? = null) {
+    private fun startBarcode(barcodeOptions: ArrayList<String>? = null) {
         val intent = Intent(this, MLKitActivity::class.java)
         intent.putExtra(MLKitActivity.MODE, Modes.BARCODE.value)
         intent.putStringArrayListExtra(MLKitActivity.BARCODE_OPTIONS, barcodeOptions)
         intent.putExtra(MLKitActivity.CONFIG, sampleConfig())
         startActivityForResult(intent, OP_MLKIT)
-    }
-
-    private fun sampleConfig() = Config(
-        branding = true,
-        background = String.empty(),
-        font = String.empty(),
-        imageResultType = imageType,
-        label = String.empty(),
-        isManualCapture = true
-    )
-
-    companion object {
-        private const val TAG = "Newlogic-MLkit"
-        private const val OP_MLKIT = 1001
-        private val imageType = PATH.value
     }
 }
