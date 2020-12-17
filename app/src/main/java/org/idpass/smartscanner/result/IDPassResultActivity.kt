e4e7badb010e349f9c4209bda79058fcc8de4eff
Reuben Mangubat <reuben@newlogic.com>
2020-12-17 12:03:20 +0800

    Add Share implementation

--- app/src/main/java/org/idpass/smartscanner/result/IDPassResultActivity.kt
+++ app/src/main/java/org/idpass/smartscanner/result/IDPassResultActivity.kt
@@ -1,8 +1,11 @@
-package org.idpass.smartscanner
+package org.idpass.smartscanner.result
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,13 +17,14 @@ import org.idpass.lite.IDPassReader
 import org.idpass.lite.exceptions.CardVerificationException
 import org.idpass.lite.exceptions.InvalidCardException
 import org.idpass.lite.exceptions.InvalidKeyException
+import org.idpass.smartscanner.R
 import org.idpass.smartscanner.lib.SmartScannerActivity
 import org.idpass.smartscanner.lib.extension.empty
 import org.idpass.smartscanner.lib.extension.hideKeyboard
 import org.idpass.smartscanner.utils.DateUtils.formatDate
 import org.idpass.smartscanner.utils.DateUtils.isValidDate
 
-class IDPassResultActivity : AppCompatActivity() {
+class IDPassResultActivity : AppCompatActivity(), View.OnClickListener {
 
     companion object {
         const val RESULT = "idpass_result"
@@ -29,33 +33,40 @@ class IDPassResultActivity : AppCompatActivity() {
 
     private var pinCode: String = ""
     private var qrBytes:ByteArray? = null
+    private var resultString : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_idpass_result)
         overridePendingTransition(R.anim.slide_in_up, android.R.anim.fade_out)
-        val toolbar : Toolbar? =findViewById(R.id.toolbar)
+        // Initialize UI
+        val toolbar : Toolbar? = findViewById(R.id.toolbar)
+        val pinCodeBtn: Button = findViewById(R.id.pinCodeAuth)
+        pinCodeBtn.setOnClickListener(this)
+        // Setup toolbar
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_close)
-        val pinCodeBtn: Button = findViewById(R.id.pinCodeAuth)
-        pinCodeBtn.setOnClickListener {
-            val cardpincode = findViewById<EditText>(R.id.cardPinCode)
-            pinCode = cardpincode.text.toString()
-            qrBytes?.let {
-                val qrstr = readCard(idPassReader, it)
-                val tv =  (findViewById<TextView>(R.id.hex))
-                tv.text = "\n" + qrstr + "\n"
-            }
-            hideKeyboard(pinCodeBtn)
-        }
-        val intent = intent
-        val qrbytes = intent.getByteArrayExtra(RESULT)
-        val qrstr = qrbytes?.let { readCard(idPassReader, it) }
+        // Display ID PASS Lite Result
+        displayResult(intent.getByteArrayExtra(RESULT))
+    }
+
+    private fun displayResult(qrbytes: ByteArray?) {
         val tv =  (findViewById<TextView>(R.id.hex))
+        val qrstr = qrbytes?.let { readCard(idPassReader, it) }
         tv.text = "\n" + qrstr + "\n"
+        resultString = getShareResult(qrstr)
+    }
+
+    private fun getShareResult(result: String?) : String {
+        val dump = StringBuilder()
+        dump.append("Scan Result via ID PASS SmartScanner:\n\n")
+        dump.append("ID PASS Lite\n")
+        dump.append("-------------------------\n")
+        dump.append(result)
+        return dump.toString()
     }
 
     private fun readCard(idPassReader: IDPassReader, qrbytes: ByteArray, charsPerLine: Int = 33): String {
@@ -131,9 +142,36 @@ class IDPassResultActivity : AppCompatActivity() {
         }
     }
 
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.pinCodeAuth -> {
+                val cardpincode = findViewById<EditText>(R.id.cardPinCode)
+                pinCode = cardpincode.text.toString()
+                displayResult(qrBytes)
+                hideKeyboard(cardpincode)
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.share_menu, menu)
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> finish()
+            R.id.share -> {
+                resultString?.let {
+                    val sendIntent: Intent = Intent().apply {
+                        action = Intent.ACTION_SEND
+                        putExtra(Intent.EXTRA_TEXT, it)
+                        type = "text/plain"
+                    }
+                    val shareIntent = Intent.createChooser(sendIntent, null)
+                    startActivity(shareIntent)
+                }
+            }
         }
         return super.onOptionsItemSelected(item)
     }
