664573c4c1486d2d230f5877801440585775f21f
Reuben Mangubat <reuben@newlogic.com>
2020-12-23 20:58:29 +0800

    Add initial Google Analytics implementation

---
+++ app/src/main/java/org/idpass/smartscanner/SmartScannerApplication.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2020 Newlogic Pte. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ */
+package org.idpass.smartscanner
+
+import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
+
+
+class SmartScannerApplication : Application() {
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
+
+    override fun onCreate() {
+        super.onCreate()
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+    }
+}
