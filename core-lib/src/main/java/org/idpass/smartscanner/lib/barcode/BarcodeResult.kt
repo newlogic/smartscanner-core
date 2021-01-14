c36e674bdab032df6ad0eff2c86cc48d23bb95af
Reuben Mangubat <reuben@newlogic.com>
2021-01-14 14:32:53 +0800

    Add barcode package and related classes

--- core-lib/src/main/java/org/idpass/smartscanner/lib/barcode/BarcodeResult.kt
+++ core-lib/src/main/java/org/idpass/smartscanner/lib/barcode/BarcodeResult.kt
@@ -16,7 +16,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-package org.idpass.smartscanner.lib.platform
+package org.idpass.smartscanner.lib.barcode
 
 data class BarcodeResult(
         val imagePath: String?,
