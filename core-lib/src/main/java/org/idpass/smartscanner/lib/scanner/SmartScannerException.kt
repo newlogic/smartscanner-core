cc39d498c67ebc59db2140c041db2e6d8ab2d422
Reuben Mangubat <reuben@newlogic.com>
2021-01-14 14:29:27 +0800

    Add scanner package and related classes

--- core-lib/src/main/java/org/idpass/smartscanner/lib/scanner/SmartScannerException.kt
+++ core-lib/src/main/java/org/idpass/smartscanner/lib/scanner/SmartScannerException.kt
@@ -15,13 +15,9 @@
  *
  *
  */
-package org.idpass.smartscanner.lib.platform
+package org.idpass.smartscanner.lib.scanner
 
-import androidx.camera.core.ImageAnalysis
-import androidx.camera.core.ImageProxy
-
-class SmartScannerAnalyzer(private var analyze: ((ImageProxy) -> Unit)) : ImageAnalysis.Analyzer {
-    override fun analyze(imageProxy: ImageProxy) {
-        analyze.invoke(imageProxy)
-    }
+class SmartScannerException : Exception {
+    constructor() : super()
+    constructor(s: String?) : super(s)
 }
