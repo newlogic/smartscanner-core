7b25a89e42be2783449235f26ff97fec185a557d
Reuben Mangubat <reuben@newlogic.com>
2021-01-16 01:03:31 +0800

    Update ID PASS Lite call in SmartScanner app

--- core-lib/src/main/java/org/idpass/smartscanner/lib/platform/BaseActivity.kt
+++ core-lib/src/main/java/org/idpass/smartscanner/lib/platform/BaseActivity.kt
@@ -17,17 +17,10 @@
  */
 package org.idpass.smartscanner.lib.platform
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 abstract class BaseActivity : AppCompatActivity() {
 
-    abstract fun layoutId(): Int
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(layoutId())
-    }
 
     fun hideActionBar() {
         supportActionBar?.hide()
