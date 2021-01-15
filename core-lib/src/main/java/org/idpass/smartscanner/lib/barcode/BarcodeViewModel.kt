81d19fb20cb3ce10c379af29c9057d46fc539106
Reuben Mangubat <reuben@newlogic.com>
2021-01-15 10:34:43 +0800

    Refactor ViewModel implementation

--- core-lib/src/main/java/org/idpass/smartscanner/lib/barcode/BarcodeViewModel.kt
+++ core-lib/src/main/java/org/idpass/smartscanner/lib/barcode/BarcodeViewModel.kt
@@ -1,41 +1,26 @@
 package org.idpass.smartscanner.lib.barcode
 
-import android.app.Application
-import android.content.Context
 import android.graphics.Bitmap
 import android.util.Log
-import androidx.annotation.MainThread
 import androidx.camera.core.ImageProxy
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
 import org.idpass.smartscanner.lib.SmartScannerActivity
-import org.idpass.smartscanner.lib.platform.extension.cacheImagePath
 import org.idpass.smartscanner.lib.platform.extension.cacheImageToLocal
 
-class BarcodeViewModel(application: Application) : AndroidViewModel(application) {
+class BarcodeViewModel : ViewModel() {
 
-    private val context: Context
-        get() = getApplication<Application>().applicationContext
-
-    private val barcodeResultData = MutableLiveData<BarcodeResult>()
-
-    @MainThread
-    fun result() : LiveData<BarcodeResult> = barcodeResultData
-
-    @MainThread
-    fun analyze(barcodeFormats : List<Int>, bf : Bitmap, imageProxy : ImageProxy) {
+    fun analyze(barcodeFormats : List<Int>, bitmap : Bitmap, filePath : String, imageProxy : ImageProxy, onResult: (BarcodeResult) -> Unit) {
         val start = System.currentTimeMillis()
         var barcodeFormat = Barcode.FORMAT_CODE_39 // Most common barcode format
         barcodeFormats.forEach {
             barcodeFormat = it or barcodeFormat // bitwise different barcode format options
         }
         val options = BarcodeScannerOptions.Builder().setBarcodeFormats(barcodeFormat).build()
-        val image = InputImage.fromBitmap(bf, imageProxy.imageInfo.rotationDegrees)
+        val image = InputImage.fromBitmap(bitmap, imageProxy.imageInfo.rotationDegrees)
         val scanner = BarcodeScanning.getClient(options)
         Log.d("${SmartScannerActivity.TAG}/SmartScanner", "barcode: process")
         scanner.process(image)
@@ -54,13 +39,12 @@ class BarcodeViewModel(application: Application) : AndroidViewModel(application)
                     }
                     cornersString = builder.toString()
                     rawValue = barcodes[0].rawValue!!
-                    val imageCachePathFile = context.cacheImagePath()
-                    bf.cacheImageToLocal(
-                        imageCachePathFile,
+                    bitmap.cacheImageToLocal(
+                        filePath,
                         imageProxy.imageInfo.rotationDegrees
                     )
-                    val barcodeResult = BarcodeResult(imageCachePathFile, cornersString, rawValue)
-                    barcodeResultData.postValue(barcodeResult)
+                    val barcodeResult = BarcodeResult(filePath, cornersString, rawValue)
+                    onResult.invoke(barcodeResult)
                 } else {
                     Log.d("${SmartScannerActivity.TAG}/SmartScanner", "barcode: nothing detected")
                 }
@@ -68,5 +52,6 @@ class BarcodeViewModel(application: Application) : AndroidViewModel(application)
             .addOnFailureListener { e ->
                 Log.d("${SmartScannerActivity.TAG}/SmartScanner", "barcode: failure: ${e.message}")
             }
+        imageProxy.close()
     }
 }
