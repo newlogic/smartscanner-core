79c9b0f4c734309ea94a92d1a871edf0084d0038
Reuben Mangubat <reuben@newlogic.com>
2021-07-16 21:35:44 +0800

    Add withMrzPhoto flag for adding MRZ Image in NFCResult

--- core-lib/src/main/java/org/idpass/smartscanner/lib/scanner/config/NFCOptions.kt
+++ core-lib/src/main/java/org/idpass/smartscanner/lib/scanner/config/NFCOptions.kt
@@ -25,6 +25,7 @@ data class NFCOptions(
     val label: String? = null,
     val locale: String? = null,
     val withPhoto: Boolean? = null,
+    val withMrzPhoto: Boolean? = null,
     val captureLog: Boolean? = null,
     val enableLogging: Boolean? = null
 ) : Parcelable {
